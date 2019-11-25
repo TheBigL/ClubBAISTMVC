cca5fd9a0941b3d8ddf5fc826abfedd71b71dce8
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-25 15:19:58 -0700

    Created the DailyTeeSheet object

--- ClubBAISTMVC/Models/DailyTeeSheet.cs
+++ ClubBAISTMVC/Models/DailyTeeSheet.cs
@@ -7,5 +7,16 @@ namespace ClubBAISTMVC.Models
 {
     public class DailyTeeSheet
     {
+        public int DailyTeeSheetID { get; set; }
+        public DateTime date { get; set; }
+        public int numberOfPlayers { get; set; }
+
+        public int numberOfCarts { get; set; }
+
+        public int golferID { get; set; }
+
+        public ICollection<TeeTime> teeTimes { get; set; }
+
+        public virtual Golfer golfer { get; set; }
     }
 }
