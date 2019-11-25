3ef0ebfaf48c137133e549cccb54bb4b9e8bbdae
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-25 16:18:06 -0700

    Added constraints to Golfer

--- ClubBAISTMVC/Models/Golfer.cs
+++ ClubBAISTMVC/Models/Golfer.cs
@@ -12,15 +12,19 @@ namespace ClubBAISTMVC.Models
         public int GolferID { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public String firstName { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public String lastName { get; set; }
 
         [Required]
+        [MaxLength(50)]
         public String email { get; set; }
 
         [Required]
+        [StringLength(10)]
         public String phoneNumber { get; set; }
 
         [Required]
