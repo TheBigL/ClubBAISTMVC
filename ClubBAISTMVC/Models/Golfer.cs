a873d6a50c47f269f6f9fe824b111a497f3c3978
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-25 14:38:10 -0700

    Golfer class done

--- ClubBAISTMVC/Models/Golfer.cs
+++ ClubBAISTMVC/Models/Golfer.cs
@@ -11,8 +11,25 @@ namespace ClubBAISTMVC.Models
         [Key]
         public int GolferID { get; set; }
 
+        [Required]
         public String firstName { get; set; }
 
+        [Required]
         public String lastName { get; set; }
+
+        [Required]
+        public String email { get; set; }
+
+        [Required]
+        public String phoneNumber { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        public int membershipID { get; set; }
+
+        public virtual Memebership membership { get; set; }
+
     }
 }
