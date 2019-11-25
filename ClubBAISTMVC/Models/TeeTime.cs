0d75e67014b866483ab5b9cd607a482d84b18fde
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-25 16:35:59 -0700

    Added TeetimeID to TeeTime

--- ClubBAISTMVC/Models/TeeTime.cs
+++ ClubBAISTMVC/Models/TeeTime.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace ClubBAISTMVC.Models
 {
     public class TeeTime
     {
+        [Key]
+        public int teetimeID { get; set; }
+        [Required]
         public DateTime teeTime { get; set; }
+        [Required]
         public int DailyTeeSheetID { get; set; }
 
         public virtual DailyTeeSheet DailyTeeSheet { get; set; }
