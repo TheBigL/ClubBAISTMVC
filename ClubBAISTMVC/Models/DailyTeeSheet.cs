3d4082e9ffb30cf901a91ce39e19f455ad274000
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-25 16:22:16 -0700

    Added requirements for the DailyTeeSheet class

--- ClubBAISTMVC/Models/DailyTeeSheet.cs
+++ ClubBAISTMVC/Models/DailyTeeSheet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,12 +8,15 @@ namespace ClubBAISTMVC.Models
 {
     public class DailyTeeSheet
     {
+        [Key]
         public int DailyTeeSheetID { get; set; }
+        [Required]
         public DateTime date { get; set; }
+        [Required]
         public int numberOfPlayers { get; set; }
-
+        [Required]
         public int numberOfCarts { get; set; }
-
+        [Required]
         public int golferID { get; set; }
 
         public virtual ICollection<TeeTime> teeTimes { get; set; }
