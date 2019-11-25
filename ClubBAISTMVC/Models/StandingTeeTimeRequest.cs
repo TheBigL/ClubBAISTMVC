a2da54d13027cd0584332786257d8634746fca7b
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-25 16:08:18 -0700

    Adding Tee Time Request

--- ClubBAISTMVC/Models/StandingTeeTimeRequest.cs
+++ ClubBAISTMVC/Models/StandingTeeTimeRequest.cs
@@ -10,19 +10,19 @@ namespace ClubBAISTMVC.Models
     {
         [Key]
         public int StandingTeeTimeRequestID { get; set; }
-
+        [Required]
         public DateTime requestedTeeTime { get; set; }
-
+        [Required]
         public DateTime startDate { get; set; }
-
+        [Required]
         public DateTime endDate { get; set; }
-
+        [Required]
         public int GolferID { get; set; }
-
+        [Required]
         public int DailyTeeSheetID { get; set; }
-
+        [Required]
         public virtual Golfer Golfer { get; set; }
-
+        [Required]
         public virtual DailyTeeSheet DailyTeeSheet { get; set; }
 
     }
