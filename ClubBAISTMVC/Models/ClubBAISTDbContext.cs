7b410b1ba7e993f9f0a4253078aa4fcd96781958
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-26 08:26:44 -0700

    Created ClubBAISTDbContext

--- ClubBAISTMVC/Models/ClubBAISTDbContext.cs
+++ ClubBAISTMVC/Models/ClubBAISTDbContext.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClubBAISTMVC.Models
 {
-    public class ClubBAISTDbContext
+    public class ClubBAISTDbContext : DbContext
     {
+        public DbSet<Golfer> Golfers { get; set; }
+
+
+        public ClubBAISTDbContext(DbContextOptions<ClubBAISTDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
     }
 }
