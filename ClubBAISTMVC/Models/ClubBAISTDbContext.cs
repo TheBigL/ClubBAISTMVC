ade759153a3f2e3f996b6c320159362960649bed
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2019-11-26 08:33:27 -0700

    Building the DBSets in the DbContext classes.

--- ClubBAISTMVC/Models/ClubBAISTDbContext.cs
+++ ClubBAISTMVC/Models/ClubBAISTDbContext.cs
@@ -10,6 +10,14 @@ namespace ClubBAISTMVC.Models
     {
         public DbSet<Golfer> Golfers { get; set; }
 
+        public DbSet<DailyTeeSheet> DailyTeeSheets { get; set; }
+
+        public DbSet<Memebership> Memeberships { get; set; }
+
+        public DbSet<StandingTeeTimeRequest> StandingTeeTimeRequests { get; set; }
+
+        public DbSet<TeeTime> TeeTimes { get; set; }
+
 
         public ClubBAISTDbContext(DbContextOptions<ClubBAISTDbContext> options) : base(options)
         {
